510ec825b2b86a2bc72b9195bec66ef626b31b25
Gabriel Hammes <gabriel.hammes@hotmail.com>
2022-11-07 15:30:36 -0300

    rotas e logs

--- lib/models/seguranca.dart
+++ lib/models/seguranca.dart
@@ -1,4 +1,5 @@
 import 'data/seguranca_data.dart';
+import '../globals.dart' as globals;
 
 class Seguranca {
   final String id;
@@ -13,6 +14,50 @@ class Seguranca {
     required this.info,
     required this.date,
     required this.tag,
+  });
+}
+
+int getIndex() {
+  var dados = globals.loginData as Map;
+  var reservas = [];
+
+  reservas = dados['hospede']['reservas'];
+  int index = reservas.indexWhere((reserva) {
+    Map mapa = reserva as Map;
+    if (mapa['reserva']['status'].toString().toLowerCase() == 'ativa') {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return index;
+}
+
+void getLog() {
+  // var logList = [];
+  // logList = globals.loginData['hospede']['reservas'][getIndex()]['reserva']
+  //     ['quarto']['registros'];
+  var logs = [];
+  logs = globals.loginData['hospede']['reservas'][getIndex()]['reserva']
+      ['quarto']['registros'];
+
+  //print(log);
+
+  for (var i = 0; i < logs.length; i++) {
+    var logMap = logs[i] as Map;
+    SEGURANCA_DATA.add(
+      Seguranca(
+        id: logMap['_id'],
+        title: 'Acesso ao quarto',
+        info: logMap.containsKey('reserva') ? 'Hóspede' : 'Funcionário',
+        date: logMap['createdAt'],
+        tag: 'tag',
+      ),
+    );
+  }
+
+  SEGURANCA_DATA.sort((a, b) {
+    return -DateTime.parse(a.date).compareTo(DateTime.parse(b.date));
   });
 }
 
